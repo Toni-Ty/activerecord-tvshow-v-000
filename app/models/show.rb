f9264a2bad71c0865dd258230350ee1ad77c7498
Toni-Ty <antonique2479@yahoo.com>
2019-08-30 05:31:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,18 +1,20 @@
 class Show < ActiveRecord::Base
 end
 
-def self::highest_rating
+def self.highest_rating
   self.maximum(:rating)
 end
 
-def self::lowest_rating
+def self.lowest_rating
   self.minimum(:rating)
 end
 
-def self::most_popular_show
+def self.most_popular_show
   self.where("rating = ?", self.highest_rating).first
 end
 
-def self::least_popular_show
+def self.least_popular_show
   self.where("rating = ?", self.lowest_rating).first
 end
+
+
