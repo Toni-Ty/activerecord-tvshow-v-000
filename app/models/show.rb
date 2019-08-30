035805ceecb657047ac4960807ad3ef33285fb1d
Toni-Ty <antonique2479@yahoo.com>
2019-08-30 05:34:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,3 +16,13 @@ end
 def self.least_popular_show
   self.where("rating = ?", self.lowest_rating).first
 end
+
+def self.ratings_sum
+  self.sum(:rating)
+end
+
+def self.shows_by_alphabetical_order
+  self.order(:name)
+end
+  
+  
